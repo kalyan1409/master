1786d28b995b99b44be4ee6dd535ffdd1bb934b0
kalyan1409 <84671131+kalyan1409@users.noreply.github.com>
2021-05-24 07:51:57 +0530

    vpc-subnet

---
+++ subnet.tf
@@ -0,0 +1,16 @@
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "private subnet"
+  }
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public subnet"
+  }
+}
