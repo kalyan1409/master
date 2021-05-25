1e57727852e2236ca491196e354aa5358f7cac05
kalyan1409 <84671131+kalyan1409@users.noreply.github.com>
2021-05-25 08:32:41 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnet1" {
   vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.1.1.0/24"
 
   tags = {
     Name = "private subnet"
@@ -8,7 +8,7 @@ resource "aws_subnet" "subnet1" {
 }
 resource "aws_subnet" "subnet2" {
   vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.1.2.0/24"
 
   tags = {
     Name = "public subnet"
