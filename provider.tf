9f36f3ce0932fc2e1c9d67c73cf8c593c4b18d3f
kalyan1409 <84671131+kalyan1409@users.noreply.github.com>
2021-05-25 08:37:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
-# Configure the AWS Provider
- provider "aws" {
-  version    = "~> 2.0"
-  region     = "ap-south-1"
-
-}
+provider "aws" {
+  region     = us-west-1
+  access_key = AKIAWVTO3PZZDQQG2IFP
+  secret_key = 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
+  }
